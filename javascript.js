1c6ab3243c20742cf1bf020b2eed0126b8d6663f
Salmane <88793063+salmaane@users.noreply.github.com>
2022-10-18 21:55:02 +0100

    Media queries started

--- javascript.js
+++ javascript.js
@@ -1,45 +1,3 @@
-// ======= Dark mode ========== //
-
-let toggle = document.querySelector(".toggle");
-toggle.onclick = function() {
-  let main = document.querySelector('main');
-  if(this.getAttribute("src") === 'images/icon-moon.svg') {
-    this.src = 'images/icon-sun.svg';
-    main.style.backgroundImage = 'url("images/bg-desktop-dark.jpg")';
-  }else {
-    this.src = 'images/icon-moon.svg';
-    main.style.backgroundImage = 'url("images/bg-desktop-light.jpg")';
-  }
-  darkMode();
-}
-
-function darkMode() {
-  document.body.classList.toggle('dark');
-
-  let newTodo = document.querySelector('.new-todo');
-  newTodo.classList.toggle('dark-new');
-
-  let list = document.querySelector('.list');
-  list.classList.toggle('dark-list');
-
-  let containers = document.querySelectorAll('.container');
-  for(let cont of containers){
-    cont.classList.toggle('dark-container');
-  }
-
-  let checkMark = document.querySelectorAll('.checkmark');
-  for(let check of checkMark) {
-    check.classList.toggle('dark-checkmark')
-  }
-
-  let filter = document.querySelector('.filter');
-  filter.classList.toggle('dark-filter');
-
-  let clear = document.querySelector('.clear');
-  clear.classList.toggle('dark-clear');
-}
-
-
 let filter = document.querySelector(".filter");
 let tasks = document.getElementsByClassName('hidden_input');
 
@@ -112,3 +70,54 @@ function createTask(value) {
             </label>`;
   list.firstElementChild.append(li);
 }
+// ======= Dark mode ========== //
+
+let toggle = document.querySelector(".toggle");
+toggle.onclick = function() {
+  let main = document.querySelector('main');
+  if(this.getAttribute("src") === 'images/icon-moon.svg') {
+    this.src = 'images/icon-sun.svg';
+    main.style.backgroundImage = 'url("images/bg-desktop-dark.jpg")';
+  }else {
+    this.src = 'images/icon-moon.svg';
+    main.style.backgroundImage = 'url("images/bg-desktop-light.jpg")';
+  }
+  darkMode();
+}
+
+function darkMode() {
+  document.body.classList.toggle('dark');
+
+  let mainSection = document.querySelector(".main-section");
+  mainSection.classList.toggle("dark-main-section");
+
+  let newTodo = document.querySelector('.new-todo');
+  newTodo.classList.toggle('dark-new');
+
+  let list = document.querySelector('.list');
+  list.classList.toggle('dark-list');
+
+  let containers = document.querySelectorAll('.container');
+  for(let cont of containers){
+    cont.classList.toggle('dark-container');
+  }
+
+  let checkMark = document.querySelectorAll('.checkmark');
+  for(let check of checkMark) {
+    check.classList.toggle('dark-checkmark')
+  }
+
+  let filter = document.querySelector('.filter');
+  filter.classList.toggle('dark-filter');
+
+  let clear = document.querySelector('.clear');
+  clear.classList.toggle('dark-clear');
+
+  let control = document.querySelector(".control");
+  control.classList.toggle("dark-control");
+
+  let filterMobile = document.querySelector(".mobile");
+  filterMobile.classList.toggle("dark-filter-mobile");
+}
+
+
